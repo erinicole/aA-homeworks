21b4ac14941a217748a29a40ba38110d6f4b2f99
Erin Hill <erinicole1988@gmail.com>
2019-07-12 07:58:27 -0700

    created map class

--- W3D5/DIY_ADTs.rb
+++ W3D5/DIY_ADTs.rb
@@ -53,3 +53,50 @@ class Stack
       self.the_queue[0]
     end
   end
+
+  class Map
+    attr_accessor :the_map
+    def initialize
+        @the_map = [] 
+    end
+
+    def set(key, value)
+      # adds an key and value to the map
+     if self.the_map.empty?
+        self.the_map << [key, value]
+     else
+      self.the_map.each do |key_v_pair|
+        if key_v_pair[0] == key
+            key_v_pair[1] = value
+            return [key_v_pair[0],key_v_pair[1]]
+         else
+           return self.the_map << [key, value]
+         end
+      end
+     end
+    end
+
+    def get(key)
+        self.the_map.each do |key_v_pair|
+            if key_v_pair[0] == key
+                return key_v_pair[1] 
+            end
+        end
+        nil
+    end
+
+    def delete(key)
+      # removes key value pair from the map
+        self.the_map.select! do |key_v_pair|
+            unless key_v_pair[0] == key
+                key_v_pair
+            end
+        end
+        self.the_map
+    end
+
+    def show
+      # returns, but doesn't remove, the first element in the stack
+      self.the_map[0]
+    end
+  end
